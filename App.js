aa6ce762b065ba33b90395c60350c626b1e25f1a
mattiasphbg@github.com <mattiasp@hotmail.com>
2021-10-27 07:24:48 +0200

    splitComponents

--- App.js
+++ App.js
@@ -4,24 +4,8 @@ import * as React from 'react';
 import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button title="Home" onPress={() => navigation.push('Home')} />
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Details')}
-      />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
-      />
-    </View>
-  );
-}
+import DetailsScreen from './screens/Details';
+import HomeScreen from './screens/Home';
 
 const Stack = createNativeStackNavigator();
 
@@ -30,7 +14,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
